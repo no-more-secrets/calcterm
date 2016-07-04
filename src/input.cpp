a2bb24ebdd54ed29d4c20bc2a04c20835d4f9288
David P. Sicilia <dpsicilia@gmail.com>
2016-07-03 23:13:17 -0400

    First working version of input

--- src/input.cpp
+++ src/input.cpp
@@ -1,57 +1,84 @@
 #include "input.hpp"
 #include "scope_exit.hpp"
-#include <stdexcept>
+#include "assert.hpp"
 
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
-
-#define ASSERT( a ) \
-    if( !(a) )        \
-        throw std::logic_error( \
-            "Assertion error:" __FILE__ ":" TOSTRING(__LINE__) \
-        );
-
-#define ASSERT_INVARIANTS                        \
-    ASSERT( width     > 0 );                     \
-    ASSERT( width % 2 == 0 );                    \
-                                                 \
-    ASSERT( start_pos >= 0 );                    \
-    ASSERT( start_pos < (int)buffer.length() );  \
-    ASSERT( start_pos % (width/2) == 0 );        \
-                                                 \
-    ASSERT( pos >= start_pos );                  \
-    ASSERT( pos <  start_pos+width );            \
+#define ASSERT_INVARIANTS                         \
+    ASSERT( width     > 0 );                      \
+    ASSERT( width % 2 == 0 );                     \
+                                                  \
+    ASSERT( start_pos >= 0 );                     \
+    ASSERT( buffer.length() == 0 ||               \
+            (start_pos < (int)buffer.length()) ); \
+    ASSERT( start_pos % (width/2) == 0 );         \
+                                                  \
+    ASSERT( pos >= start_pos );                   \
+    ASSERT( pos <  start_pos+width );             \
     ASSERT( pos <= (int)buffer.length() );
-
-#define SCOPE_ASSERT SCOPE_EXIT( ASSERT_INVARIANTS )
 
 Input::Input(int w) : width(w)
                     , buffer("")
                     , start_pos(0)
                     , pos(0) {
-    SCOPE_ASSERT
-}
-
-void Input::update_start() {
-    SCOPE_ASSERT
-    start_pos = (pos/(width/2))*(width/2);
-}
-
-void Input::paste( std::string const& new_string ) {
-    SCOPE_ASSERT
-    buffer.insert( pos, new_string );
-    pos += new_string.length();
+    ASSERT_INVARIANTS
 }
 
 void Input::draw( int y, int x ) const {
-
+    std::string out = (char const*)&buffer[start_pos];
+    int residual = buffer.length()-start_pos;
+    residual = (residual >= width) ? 0 : width-residual;
+    out += std::string( residual, ' ' );
+    mvaddnstr( y, x, out.c_str(), width );
 }
 
 int Input::get_cursor() const {
     return pos-start_pos;
 }
 
+void Input::update_start() {
+    if( pos >= start_pos && pos < start_pos+width )
+        return;
+    start_pos = (pos/(width/2))*(width/2);
+    if( start_pos == pos )
+        start_pos -= width/2;
+    if( start_pos < 0 )
+        start_pos = 0;
+    ASSERT_INVARIANTS
+}
+
+void Input::paste( std::string const& new_string ) {
+    buffer.insert( pos, new_string );
+    pos += new_string.length();
+    ASSERT_INVARIANTS
+    update_start();
+    ASSERT_INVARIANTS
+}
+
 bool Input::key_press( int pressed ) {
-    SCOPE_ASSERT
+    if( (pressed >= 'a' && pressed <= 'z') ||
+        (pressed >= 'A' && pressed <= 'Z') ||
+        (pressed >= '0' && pressed <= '9') ||
+        (pressed == ' ') ||
+        (pressed == '+') ||
+        (pressed == '-') ||
+        (pressed == '*') ||
+        (pressed == '/') ||
+        (pressed == '^') ||
+        (pressed == '(') ||
+        (pressed == ')')) {
+        buffer.insert(pos, 1, (char)pressed);
+        ++pos;
+        update_start();
+    }
+    else if( pressed == KEY_LEFT ) {
+        --pos;
+        pos = (pos < 0) ? 0 : pos;
+        update_start();
+    }
+    else if( pressed == KEY_RIGHT ) {
+        ++pos;
+        pos = (pos > (int)buffer.length()) ? buffer.length() : pos;
+        update_start();
+    }
+    ASSERT_INVARIANTS
     return true;
 }
