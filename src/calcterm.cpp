22fb98d70871ca7948dd0875094d8a8f46720942
David P. Sicilia <dpsicilia@gmail.com>
2016-07-04 00:08:15 -0400

    Basic ui mechanism in place except for scrolling of history

--- src/calcterm.cpp
+++ src/calcterm.cpp
@@ -5,6 +5,11 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stdexcept>
+#include <iostream>
+
+#include "scope_exit.hpp"
+#include "input.hpp"
 
 struct Entry
 {
@@ -36,16 +41,16 @@ auto render_stripe( int width, Stripe const& s ) -> std::vector<std::string>
 auto draw_stripe( int width, int start_y, bool highlight, Stripe const& s ) -> void
 {
     auto text = render_stripe( width, s );
+    if( highlight ) attron( A_REVERSE );
     for( int i = text.size(); i > 0; --i ) {
         if( start_y-i < 0 )
             break;
-        if( highlight ) attron( A_REVERSE );
         mvprintw( start_y-text.size()+i, 0, text[i-1].c_str() );
-        attroff( A_REVERSE );
     }
+    if( highlight ) attroff( A_REVERSE );
 }
 
-auto draw_stripes( int highlight, std::vector<Stripe> v ) -> void
+auto draw_stripes( int highlight, std::vector<Stripe> const& v ) -> void
 {
     int height = 0, width = 0;
     getmaxyx( stdscr, height, width );
@@ -92,25 +97,34 @@ std::vector<std::string> s3 = {
     "\\                  / \\                 /  "
 };
 
+std::vector<std::string> s4 = { "1" };
+
+std::vector<std::string> s5 = { "1.000000000000000000000000000000000000000000000000" };
+
 std::vector<Stripe> vs = {
-    { {"",s2}, false,  (int)s2.size(), (int)s2[0].length() },
-    { {"",s3}, true,   (int)s3.size(), (int)s3[0].length() },
-    { {"",s1}, false,  (int)s1.size(), (int)s1[0].length() },
-    { {"",s1}, true,   (int)s1.size(), (int)s1[0].length() },
-    { {"",s3}, false,  (int)s3.size(), (int)s3[0].length() },
-    { {"",s2}, true,   (int)s2.size(), (int)s2[0].length() },
-    { {"",s2}, false,  (int)s2.size(), (int)s2[0].length() },
-    { {"",s1}, true,   (int)s1.size(), (int)s1[0].length() },
-    { {"",s1}, false,  (int)s1.size(), (int)s1[0].length() },
-    { {"",s2}, true,   (int)s2.size(), (int)s2[0].length() },
-    { {"",s3}, false,  (int)s3.size(), (int)s3[0].length() },
-    { {"",s3}, true,   (int)s3.size(), (int)s3[0].length() },
-    { {"",s1}, false,  (int)s1.size(), (int)s1[0].length() },
+    { {"123",s2}, false,  (int)s2.size(), (int)s2[0].length() },
+    { {"123",s3}, true,   (int)s3.size(), (int)s3[0].length() },
+    { {"123",s1}, false,  (int)s1.size(), (int)s1[0].length() },
+    { {"123",s1}, true,   (int)s1.size(), (int)s1[0].length() },
+    { {"123",s3}, false,  (int)s3.size(), (int)s3[0].length() },
+    { {"123",s1}, true,   (int)s1.size(), (int)s1[0].length() },
+    { {"123",s2}, false,  (int)s2.size(), (int)s2[0].length() },
+    { {"123",s1}, true,   (int)s1.size(), (int)s1[0].length() },
+    { {"123",s1}, false,  (int)s1.size(), (int)s1[0].length() },
+    { {"123",s2}, true,   (int)s2.size(), (int)s2[0].length() },
+    { {"123",s3}, false,  (int)s3.size(), (int)s3[0].length() },
+    { {"123",s3}, true,   (int)s3.size(), (int)s3[0].length() },
+    { {"123",s1}, false,  (int)s1.size(), (int)s1[0].length() },
+    { {"123",s4}, true,   (int)s4.size(), (int)s4[0].length() },
+    { {"123",s5}, false,  (int)s5.size(), (int)s5[0].length() },
 };
 
-int main()
+int _main(int argc, char* argv[])
 {
     initscr();
+    SCOPE_EXIT( endwin() )
+    //raw();
+    //nonl();
     noecho();
     keypad(stdscr, TRUE);
     draw_stripes( -1, vs );
@@ -118,17 +132,57 @@ int main()
     int highlight = -1;
     int height = 0, width = 0;
     getmaxyx( stdscr, height, width );
+    Input in( width-1 );
+    bool editing = true;
     while( (ch = getch()) != (int)'q' )
     {
         switch( ch )
         {
-            case KEY_UP:   highlight += 1; break;
-            case KEY_DOWN: highlight -= 1; break;
+            case KEY_UP:
+                highlight += 1;
+                editing = false;
+                break;
+            case KEY_DOWN:
+                highlight -= 1;
+                if( highlight == -1 )
+                    editing = true;
+                break;
+            case '\n': {
+                if( editing ) {
+                    std::string const& str = in.get_string();
+                    if( !str.empty() ) {
+                        Stripe s({ {str,{str}}, true, (int)1, (int)str.length() });
+                        vs.push_back( s );
+                        in.clear();
+                    }
+                }
+                else {
+                    std::string to_insert = vs[vs.size() - highlight - 1].e.one_line; 
+                    in.paste( to_insert );
+                    highlight = -1;
+                    editing = true;
+                }
+                break;
+            }
+            default:
+                if( editing )
+                    in.key_press( ch );
+                break;
         };
         draw_stripes( highlight, vs );
-        move( height-1, 0 );
+        in.draw( height-1, 0 );
+        move( height-1, 0+in.get_cursor() );
         //refresh();
     }
-    endwin();
     return 0;
+}
+
+int main( int argc, char* argv[] )
+{
+    try {
+        return _main(argc, argv);
+    } catch( std::exception const& e ) {
+        std::cout << "exception:" << e.what() << std::endl;
+        return 1;
+    }
 }
