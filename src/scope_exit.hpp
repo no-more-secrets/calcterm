fc5252d5c98260b8c9b6b6dec6b553b04b2b67da
David P. Sicilia <dpsicilia@gmail.com>
2016-07-03 20:30:47 -0400

    Add scope_exit.hpp; contains simple macro for easy RAII

---
+++ src/scope_exit.hpp
@@ -0,0 +1,18 @@
+template<typename T>
+struct ScopeExit {
+    ScopeExit(T f) : f(f) {}
+    ~ScopeExit() { f(); }
+    ScopeExit(ScopeExit const&)  = delete;
+    ScopeExit(ScopeExit const&&) = delete;
+    ScopeExit const& operator= (ScopeExit const&)  = delete;
+    ScopeExit const& operator= (ScopeExit const&&) = delete;
+    T f;
+};
+
+#define STRING_JOIN(arg1, arg2) arg1 ## arg2
+#define SCOPE_EXIT(code)                                        \
+    auto STRING_JOIN(scope_exit_f_, __LINE__) = [&](){ code; }; \
+    ScopeExit<decltype(STRING_JOIN(scope_exit_f_, __LINE__))>   \
+        STRING_JOIN(scope_exit_, __LINE__)(                     \
+            STRING_JOIN(scope_exit_f_, __LINE__)                \
+        );
