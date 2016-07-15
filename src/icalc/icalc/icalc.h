f4370ff2104c52e41aa4a01935f22aae342e07f7
David P. Sicilia <dpsicilia@gmail.com>
2016-07-14 22:32:52 -0400

    Add a basic interface for calcterm

--- src/icalc/icalc/icalc.h
+++ src/icalc/icalc/icalc.h
@@ -1,6 +1,23 @@
+/*
+ * C Interface between calcterm and a calculator.
+ * A shared library must implement this interface
+ * to be loadable by calcterm.
+ */
 
-typedef CI_Config void*;
+extern "C" {
 
+struct CI_Config {
+    int flag;
+};
 
+struct CI_Result {
+    char*  one_line;
+    char** grid;
+    int    y;
+};
 
-CI_Result CI_submit(char const* input);
+void         CI_init( CI_Config* config );
+CI_Result*   CI_submit( char const* input );
+void         CI_result_release( CI_Result* result );
+
+} /* extern "C" */
