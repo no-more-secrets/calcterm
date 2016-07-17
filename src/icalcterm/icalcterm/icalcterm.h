3412360dcb91f27d64f24412723bd6e8cea87504
David P. Sicilia <dpsicilia@gmail.com>
2016-07-17 17:21:14 -0400

    New interface for icalcterm; not yet implemented

--- src/icalcterm/icalcterm/icalcterm.h
+++ src/icalcterm/icalcterm/icalcterm.h
@@ -6,23 +6,48 @@
 
 extern "C" {
 
-struct CI_Config {
+typedef struct _CI_Config {
+
     int flag;
-};
 
-struct CI_Result {
-    char*  one_line;
-    char** grid;
-    int    y;
-};
+}   CI_Config;
 
-typedef      void (*CI_init_t)( CI_Config* );
-void         CI_init( CI_Config* config );
+/*
+ * Holds all of the info that you get back from the
+ * calculator.  These must be freed by calling the
+ * CI_result_free function.
+ */
+typedef struct _CI_Result {
 
-typedef      CI_Result* (*CI_submit_t)( char const* );
-CI_Result*   CI_submit( char const* input );
+    char*  input_one_line;
+    char*  output_one_line;
 
-typedef      void (*CI_result_release_t)( CI_Result* result );
-void         CI_result_release( CI_Result* result );
+    char** input_grid;
+    char** output_grid;
+
+    int    input_grid_rows;
+    int    output_grid_rows;
+
+}   CI_Result;
+
+/*
+ * Initialize the calculator
+ */
+typedef void       (*CI_init)( CI_Config* );
+
+/*
+ * Set the configuration of the calculator
+ */
+typedef void       (*CI_config)( CI_Config* );
+
+/*
+ * Submit a string for processing
+ */
+typedef CI_Result* (*CI_submit)( char const* );
+
+/*
+ * This must be used to free a CI_Result object
+ */
+typedef void       (*CI_result_free)( CI_Result* result );
 
 } /* extern "C" */
