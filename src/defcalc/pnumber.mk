8086ba5e36ab0c15b283bcd13b012a309b78cb29
David P. Sicilia <dpsicilia@gmail.com>
2016-07-24 19:52:26 -0400

    Rename generated flex/bison files

--- src/defcalc/pnumber.mk
+++ src/defcalc/pnumber.mk
@@ -1,36 +1,36 @@
 stem = pnumber
 
-all: $(stem).yy.cpp $(stem).tab.cpp
+all: $(stem).l.cpp $(stem).y.cpp
 
-$(stem).tab.cpp: $(stem).y
+$(stem).y.cpp: $(stem).y
 	bison -d -o $@ $<
 
 # The `sed` step is to make a change that suppresses a warning.
 # This is supposed to be corrected in more recent versions of flex.
-$(stem).yy.cpp: $(stem).l
+$(stem).l.cpp: $(stem).l
 	flex --posix -s -I -o $@ $<
 	sed -i 's/yy_size_t yy_buf_size;/int yy_buf_size;/' $@
 
 clean:
-	-rm $(stem).yy.cpp
-	-rm $(stem).tab.hpp
-	-rm $(stem).tab.cpp
-	-rm $(stem).yy.o
-	-rm $(stem).yy.d
-	-rm $(stem).tab.o
-	-rm $(stem).tab.d
+	-rm $(stem).l.cpp
+	-rm $(stem).y.hpp
+	-rm $(stem).y.cpp
+	-rm $(stem).l.o
+	-rm $(stem).l.d
+	-rm $(stem).y.o
+	-rm $(stem).y.d
 	-rm $(stem)
 
 # ===============================================================
 # Only used to development / debugging
 # ===============================================================
-$(stem): $(stem).yy.o $(stem).tab.o
+$(stem): $(stem).l.o $(stem).y.o
 	g++ -g -o $@ $^
 
-$(stem).yy.o: $(stem).yy.cpp $(stem).tab.cpp
+$(stem).yy.o: $(stem).l.cpp $(stem).y.cpp
 	g++ -g -c $<
 
-$(stem).tab.o: $(stem).tab.cpp
+$(stem).tab.o: $(stem).y.cpp
 	g++ -g -c $<
 
 .PHONY: all clean
